d39e9f936a39caa3bcad2f8b088e35f24d275401
AYMOZ018 <arianantuanettemarcatinco@gmail.com>
2025-05-28 18:58:21 -0500

    commit prueba

--- views/registrarse.php
+++ views/registrarse.php
@@ -5,15 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <link rel="stylesheet" href="./views/index.css">
+    <link rel="stylesheet" href="registrarse.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" 
     integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Iniciar Sesion</title>
+    <title>Registrarse</title>
 </head>
 <body>
     <div class="form">
-        <form action="crud.php?c=usuario&a=iniciar" method="POST">
-            <h1>Iniciar Sesion</h1>
+        <form action="crud.php?c=usuario&a=registrarse" method="POST">
+            <h1>Registrarse</h1>
 
             <div class="mensaje">
                 <?php
@@ -24,7 +24,14 @@
                     }
                 ?>
                 <?php
-                    if (isset($_GET['error'])) {
+                    if(isset($_GET['success'])){
+                    ?>
+                        <p class="success"><?php echo $_GET['success'] ?></p>
+                    <?php     
+                    }
+                ?>
+                <?php
+                    if (isset($_GET['error']) || isset($_GET['success'])) {
                         $url = strtok($_SERVER["REQUEST_URI"], '?'); // Elimina los parámetros
                         echo "<script>history.replaceState({}, '', '$url');</script>";
                     }
@@ -34,18 +41,23 @@
             <div class="text">
                 <i class="fa-solid fa-user"></i>
                 <label for="">Usuario:</label>
-                <input type="text" placeholder="Ingrese su usuario..." name="username">
+                <input type="text" placeholder="Ingrese un usuario..." name="username">
             </div>
             <div class="text">
-                <i class="fa-solid fa-unlock"></i>
+                <i class="fa-solid fa-unlock-keyhole"></i>
                 <label for="">Contraseña:</label>
-                <input type="password" placeholder="Ingrese su contraseña..." name="password">
+                <input type="password" placeholder="Ingrese una contraseña..." name="password">
             </div>
-            
-            <button type="submit" class="btn">Iniciar Sesión</button>
+            <div class="text">
+                <i class="fa-solid fa-key"></i>
+                <label for="">Confirmar Contraseña:</label>
+                <input type="password" placeholder="Ingrese confirmacion de contraseña..."  name="RPassword">
+            </div>
+
+            <button type="submit" class="btn">Registrarse</button>
             
             <div class="link">
-                <p>No tiene una cuenta? <a href="./views/registrarse.php">Registrarse</a></p>
+                <p>Ya tiene una cuenta? <a href="iniciarsesion.php">Iniciar Sesion</a></p>
             </div>
         </form>
     </div>
