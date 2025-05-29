9c3b43ce9e7cfc1b734ef81b82aba719e3226303
4de4 <flaviochepe@gmail.com>
2025-05-29 16:31:04 -0500

    Menu

---
+++ views/menu.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu RRHH</title>
+    <link rel="stylesheet" href="menu.css">
+</head>
+<body>
+    <header>
+    <div class="menu">
+      <nav>
+          <ul>
+            <li><a href="crud.php?c=empleado&a=index">Empleados</a></li>
+            <li><a href="crud.php?c=departamento&a=index">Departamentos</a></li>
+            <li><a href="crud.php?c=configuracion&a=index">Configuracion</a></li>
+          </ul>
+      </nav>
+    </div>
+  </header>
+</body>
+</html>
